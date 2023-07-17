0dca24c865e7bacc587367fa7ea0afac0288fd5c
Sherzod Toshpulatov <sherzodtoshpolatov5@gmail.com>
2023-07-17 19:16:55 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for item in question_data:
+    question_bank.append(Question(item["text"], item["answer"]))
+
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+else:
+    print(f"You have complete quiz!\n Your final score {quiz.score} / {quiz.question_number}")
+
+
+
